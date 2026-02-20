5e861c66768d869160246f9228aad7160a4f8f08
shizomorfizm <doktorkto223455@yandex.ru>
2026-02-21 01:47:15 +0300

    Finally made window, that was hard

--- window/main.cpp
+++ window/main.cpp
@@ -2,14 +2,58 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
 int main() {
+    // Инициализация GLFW
+    if (!glfwInit()) {
+        std::cout << "Failed to init GLFW" << std::endl;
+        return -1;
+    }
     
-    glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
+    
+    // Создание окна
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
+    if (!window) {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    // Инициализация GLAD
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    
+    // Устанавливаем цвет очистки
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    
+    // Главный цикл
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        processInput(window);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
     return 0;
+}
 
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
 }
